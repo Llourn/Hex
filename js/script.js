a20116864b1470f0bfdd96ebbf5302381ff9df5a
Lorne <Lorne.S.Cyr@gmail.com>
2019-12-03 11:33:18 -0400

    Fitting together
    updated the generation code. Aligning hexagons correctly.

--- js/script.js
+++ js/script.js
@@ -10,23 +10,16 @@ $(document).ready(function () {
 function generateGrid() {
     console.log("width: " +containerWidth);
     console.log("height: " +containerHeight);
+
     // for loop of rows.
     for(let i = 1; i < containerHeight; i++) {
         console.log('ROW: ' + i);
+        $('#container').append("<div id='row-" + i + "'>");
+        // inner for loop of columns.
         for (let j = 1; j < containerWidth; j++) {
             console.log('COLUMN: ' + j);
-            $('body').append("<svg viewbox='0 0 60 60'><path class='hex' id='" + j + "-" + i + "' d='M30 0L0 15L0 45L30 60L60 45L60 15L30 0Z'/></svg>");
+            $('#row-'+i).append("<svg viewbox='0 0 60 60'><path class='hex' id='" + j + "-" + i + "' d='M30 0L0 15L0 45L30 60L60 45L60 15L30 0Z'/></svg>");
         }
-    }
-        // inner for loop of columns.
-}
-
-function generateColumns() {
-    // append div for containing row, and then append svgs as children of the row div.
-    var bodyWidth = $('body').width();
-    for(let i = 0; i < bodyWidth-120; i+=120)
-    {
-        
     }
 }
 
